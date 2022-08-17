4b2ce9c54e18dc53c4b357b101ffa83c40b31861
Bruno-Ferr <fbruno233@gmail.com>
2022-08-17 12:23:39 -0300

    create new todo, todos counter, delete todo

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,10 @@ type TodosProps = {
 }
 
 function App() {
-  const [todos, setTodos] = useState<TodosProps[] | []>([{id: 1, done: true, description: 'Beijar a ana'}, {id: 2, done: false, description: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.'}])
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState<TodosProps[] | []>([{id: 1, done: true, description: 'almoçar'}, {id: 2, done: false, description: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.'}])
+  const [doneTasks, setDoneTasks] = useState(0)
+  const [tasks, setTasks] = useState(todos.length)
+  const [description, setDescription] = useState('')
 
   function handleDone(id: number) {
     const [todo] = todos.filter(todo => todo.id === id)
@@ -21,15 +23,32 @@ function App() {
   }
 
   function handleTrash(id: number) {
-    const [todo] = todos.filter(todo => todo.id !== id)
-    setTodos([todo])
+    const todo = todos.filter(todo => todo.id !== id)
+    setTodos(todo)
+  }
+
+  function createNewTodo() {
+    const id = todos.length + 1
+    const newTodo = {
+      id,
+      done: false,
+      description
+    }
+
+    setTodos([...todos, newTodo])
+    setDescription('')
   }
 
   useEffect(() => {
-    setCount(() => {
+    setDoneTasks(() => {
       return 0
+    });
+
+    setTasks(() => {
+      return todos.length
     })
-    todos.map(todo => todo.done === true && setCount(state => { return state + 1}))
+
+    todos.map(todo => todo.done === true && setDoneTasks(state => { return state + 1}))
   }, [todos])
 
   return (
@@ -39,18 +58,23 @@ function App() {
         <h1 className={styles.title}>to<span>do</span></h1>
       </header>
       <div className={styles.newTask}>
-        <input type="text" placeholder='Adicione uma nova tarefa' />
-        <button>Criar <PlusCircle size={16}/></button>
+        <input 
+          value={description} 
+          onChange={event => setDescription(event.target.value)} 
+          type="text" 
+          placeholder='Adicione uma nova tarefa' 
+        />
+        <button onClick={() => createNewTodo()}>Criar <PlusCircle size={16}/></button>
       </div>
       <main className={styles.body}>
         <div className={styles.info}>
           <div className={styles.created}>
             <h3>Tarefas criadas</h3>
-            <div className={styles.counter}>0</div>
+            <div className={styles.counter}>{tasks}</div>
           </div>
           <div className={styles.done}>
             <h3>Concluídas</h3> 
-            <div className={styles.counter}>{count}</div>
+            <div className={styles.counter}>{doneTasks}</div>
           </div>     
         </div>
         <div>
@@ -59,20 +83,24 @@ function App() {
               todos.map(todo => 
                 todo.done === true ? (
                   <div className={styles.taskDone} key={todo.id}>
-                    <button onClick={() => handleDone(todo.id)} className={styles.checked}>
-                      <CheckCircle weight='fill' size={24} /> 
-                    </button>
-                    <p>{todo.description}</p>
+                    <div>
+                      <button onClick={() => handleDone(todo.id)} className={styles.checked}>
+                        <CheckCircle weight='fill' size={24} /> 
+                      </button>
+                      <p>{todo.description}</p>
+                    </div>
                     <button onClick={() => handleTrash(todo.id)} className={styles.trash}> 
                       <Trash size={24} />
                     </button>
                   </div>
                 ) : (
                   <div className={styles.task} key={todo.id}>
-                    <button onClick={() => handleDone(todo.id)} className={styles.check}>
-                      <Circle size={24} />
-                    </button>
-                    <p>{todo.description}</p>
+                    <div>
+                      <button onClick={() => handleDone(todo.id)} className={styles.check}>
+                        <Circle size={24} />
+                      </button>
+                      <p>{todo.description}</p>
+                    </div>
                     <button onClick={() => handleTrash(todo.id)}  className={styles.trash}> 
                       <Trash size={24} />
                     </button>
